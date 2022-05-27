664cd52383a07f75e5fa2f948bb3f5362d522637
Elena Radu <lena@Air-de-Elena.home>
2022-05-27 21:46:13 +0200

    estilo js input

--- Pago/pago.js
+++ Pago/pago.js
@@ -1,28 +1,35 @@
 
-
-//console.log(numTarjeta.split('').length);
-
-
-
 function pagar () {
     const numTarjeta = document.getElementById('numeroTarjeta').value;
-    if(numTarjeta.match('[0-9]{16}')) {
+    const inputTarjeta = document.getElementById('numeroTarjeta');
+
+    if(numTarjeta.match(/^[0-9]{16}$/)) {
         console.log("Tarjeta valida");
+        inputTarjeta.style.borderColor = "blue";
     } else {
+        inputTarjeta.style.borderColor = "red";
         console.log("Tarjeta no valida");
     }
 
     const caducidadTarjeta = document.getElementById('cadTarjeta').value;
-    if(caducidadTarjeta.match('[0-9]{2}/[0-9]{2}')){
+    const inputCaducidad = document.getElementById('cadTarjeta');
+
+    if(caducidadTarjeta.match(/^[0-9]{2}-[0-9]{2}$/)){
         console.log("Caducidad valida");
+        inputCaducidad.style.borderColor = "blue";
     } else {
-        console.log("Caduidad no valida");
+        inputCaducidad.style.borderColor = "red";
+        console.log("Caducidad no valida")
     }
     
     const cvv = document.getElementById('cvv').value;
-    if(cvv.match(['[0-9]{3}'])) {
+    const inputCvv = document.getElementById('cvv');
+
+    if(cvv.match(/^[0-9]{3}$/)) {
         console.log("CVV valido");
+        inputCvv.style.borderColor = "blue";
     } else {
-        console.log("CVV no valido");
+        inputCvv.style.borderColor = "red";
+        console.log("CVV no valido")
     }
-}
+};
