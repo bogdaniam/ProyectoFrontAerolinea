57efa559c1e765d82026ed7dedf09b1aa532eed2
bogdaniam <mihai.bogdan.mardale@gmail.com>
2022-05-27 21:15:45 +0200

    Creada funcion para historial

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     var log = JSON.parse(localStorage.sesionLogin).login;
-    console.log(JSON.parse(localStorage.sesionLogin).user)
+    //console.log(JSON.parse(localStorage.sesionLogin).user)
 
 
     if(log){
@@ -11,7 +11,7 @@ window.onload = () => {
         document.getElementById("connectD").style.display = "none"
         document.getElementById("registerD").style.display = "none"
         document.getElementById("contactD").style.display = "none"
-        document.getElementById('nameD').innerHTML = Bienvenido `${UsuarioLogeado.nombre}`
+        document.getElementById('nameD').innerHTML = `Bienvenido ${UsuarioLogeado.nombre}`
     } else {
         document.getElementById("nameD").style.display = "none";
         document.getElementById("disconnectD").style.display = "none";
@@ -19,6 +19,8 @@ window.onload = () => {
         document.getElementById("registerD").style.display = "block"
         document.getElementById("contactD").style.display = "block"
         document.getElementById("historialS").style.display = "none"
+
+
         //document.getElementById('navSinLoginD').style.visibility = "visible";
         //document.getElementById('navConLoginD').style.visibility = "hidden"
 
@@ -108,6 +110,46 @@ function desconectarD (){
     }))
 }
 
+function historyM(){
+    var logeadoM = JSON.parse(localStorage.sesionLogin).user;
+    let busquedaHistorialM = JSON.parse(localStorage.getItem("Historial"))
+    let contadorBusquedaUserM = 1;
+    //console.log(logeadoM)
+    while (contadorBusquedaUserM < busquedaHistorialM.length)  {
+        
 
+        if ((busquedaHistorialM[contadorBusquedaUserM].user) == logeadoM ) {
+            
+            var texto = (`Origen: ${busquedaHistorialM[contadorBusquedaUserM].origen}  
+            Destino: ${busquedaHistorialM[contadorBusquedaUserM].destino} 
+            Fecha ida: ${busquedaHistorialM[contadorBusquedaUserM].fechaIda}    
+            Fecha vuelta: ${busquedaHistorialM[contadorBusquedaUserM].fechaVuelta}  
+            Asientos: ${busquedaHistorialM[contadorBusquedaUserM].asientos} 
+            Precio: ${busquedaHistorialM[contadorBusquedaUserM].precio}`); 
+            
+            var etiqueta = document.createElement("p"); 
+            
+            var contenido = document.createTextNode (texto); 
+            
+           
+            etiqueta.appendChild (contenido); 
+            document.getElementById("cajaGraciasM").appendChild(etiqueta); 
+            var saltoLineaM = document.createElement("br");
+            document.getElementById("cajaGraciasM").appendChild(saltoLineaM);
+
+            
+
+            
+         }
+         contadorBusquedaUserM++;
+     }
+     
+     
+
+
+    console.log(busquedaHistorialM[1].user)  //para busca en toso los usuarios
+    //console.log(busquedaHistorialM)   //para ver todos los historiales
+
+}
 
 
